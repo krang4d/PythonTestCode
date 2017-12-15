94e974476af1e303f3aa6e826dd6c06f7d2e454e
P.G.Golovkin <jzi@inbox.ru>
2017-12-15 16:30:05 +0300

    new file:   Socket/TCPClient.py 	new file:   Socket/TCPServer.py 	new file:   hello.py

---
+++ hello.py
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/env python
+#coding: utf-8
+import urllib.request
+
+i = 5
+
+print ( u"Hello%d, World!"  %i, u"Привет, Мир!" )
+print ( type(i) )
+
+import keyword; print ( keyword.kwlist)
+
+
+url = 'http://www.ya.ru'
+r = urllib.request.get(url)
+with open('test.html', 'w') as output_file:
+  output_file.write(r.text.encode('cp1251'))
+
